6a689ab775383567748769ccea61fa6a652a6d7a
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2015-12-23 10:03:40 +0100

    Project init

---
+++ src/Catharsium.SmooStone/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catharsium.SmooStone
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Catharsium.SmooStone/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Catharsium.SmooStone")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Catharsium.SmooStone")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d0e3cf41-f5a5-477e-be67-97ee775c927e")]
